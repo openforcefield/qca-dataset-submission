42ed0eda1fb3d570bd6089e88925c8b0dce36430
Daniel Smith <malorian@me.com>
2019-07-28 12:31:43 -0400

    Updates eMolecules submission script

--- 2019-07-05 eMolecules force field discrepancies 1/02_create_optimization_dataset.py
+++ 2019-07-05 eMolecules force field discrepancies 1/02_create_optimization_dataset.py
@@ -5,9 +5,12 @@ import json
 import tqdm
 import tarfile
 from collections import Counter
-import qcportal as ptl
+#import qcportal as ptl
+import qcfractal.interface as ptl
 from qcelemental.models import Molecule
 
+
+UPDATE = True
 
 def read_molecules(input_json):
     """ Extract the molecules and the index of them from the input json file
@@ -65,39 +68,67 @@ def read_molecules(input_json):
         cmiles_ids = mdata['cmiles_identifiers']
         index = cmiles_ids['canonical_isomeric_smiles']
         for i_conformer, initial_molecule in enumerate(initial_molecules):
-            qcel_molecule = Molecule.from_data(initial_molecule)
+#            qcel_molecule = Molecule.from_data(initial_molecule)
             # use count to generate unique index
             index_count = index_counter[index]
             this_index = f'{index}-{index_count}'
             index_counter[index] += 1
             assert this_index not in molecules_dict, f"Multiple molecules have the same index, please check {mdata}"
-            molecules_dict[this_index] = qcel_molecule
+            molecules_dict[this_index] = initial_molecule
             molecule_attributes[this_index] = cmiles_ids
     return molecules_dict, molecule_attributes
 
 
 print("Extracting molecules...")
-molecules_dict, molecule_attributes = read_molecules("optimization_inputs.json.gz")
+molecules_dict, molecule_attributes = read_molecules("optimization_inputs.json")
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+molecules_dict_chunks = [dict(x) for x in chunks(list(molecules_dict.items()), 100)]
+#molecules_dict_chunks = molecules_dict_chunks[:1]
 
 print("Initializing dataset...")
-client = ptl.FractalClient("localhost:7777", verify=False) # TODO: Should this be changed to remote address?
+client = ptl.FractalClient.from_file()
+#client = ptl.FractalClient("localhost:7777", verify=False) # TODO: Should this be changed to remote address?
 
 # create a new dataset with specified name
-ds = ptl.collections.OptimizationDataset("OpenFF Discrepancy Benchmark 1", client=client)
-
-# create specification for this dataset
-opt_spec = {"program": "geometric"}
-qc_spec = {"driver": "gradient", "method": "B3LYP-d3bj", "basis": "dzvp", "program": "psi4"}
-ds.add_specification("default", opt_spec, qc_spec, description="Standard OpenFF optimization quantum chemistry specification.")
+if UPDATE:
+    ds = client.get_collection("OptimizationDataset", "OpenFF Discrepancy Benchmark 1")
+else:
+    ds = ptl.collections.OptimizationDataset("OpenFF Discrepancy Benchmark 1", client=client)
+    
+    # create specification for this dataset
+    opt_spec = {"program": "geometric"}
+    qc_spec = {"driver": "gradient", "method": "B3LYP-d3bj", "basis": "dzvp", "program": "psi4"}
+    ds.add_specification("default", opt_spec, qc_spec, description="Standard OpenFF optimization quantum chemistry specification.")
+    ds.save()
 
 # add molecules
 print(f"Adding {len(molecules_dict)} molecules")
-for molecule_index, molecule in tqdm.tqdm(molecules_dict.items()):
-    attributes = molecule_attributes[molecule_index]
-    ds.add_entry(molecule_index, molecule, attributes=attributes)
+for molecules_chunk in tqdm.tqdm(molecules_dict_chunks):
+
+    found = False
+    for molecule_index, molecule in molecules_chunk.items():
+        try:
+            attributes = molecule_attributes[molecule_index]
+            ds.add_entry(molecule_index, molecule, attributes=attributes, save=False)
+            found = True
+        except KeyError:
+            continue
+
+    if found:
+        ds.save()
 
 print("Submitting tasks...")
-comp = ds.compute("default", tag="openff", priority="normal")
-print(comp)
+responses = []
+for molecules_chunk in tqdm.tqdm(molecules_dict_chunks):
+    block = list(molecules_chunk.keys())
+    comp = ds.compute("default", tag="openff", priority="normal", subset=block)
+    responses.append(comp)
+
+# Merge responses
+
+print(f"Submitted: {sum(responses)}")
 
 print("Complete!")
