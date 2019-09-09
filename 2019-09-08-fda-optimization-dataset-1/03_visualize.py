d20de81980e66866778675f4bc6cf9eb10703ca8
John Chodera <john.chodera@choderalab.org>
2019-09-09 00:51:15 -0400

    Add FDA subset from ZINC, filtered, as an OptimizationDataset

---
+++ 2019-09-08-fda-optimization-dataset-1/03_visualize.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+"""
+Generate a PDF of all small molecules in the JSON dataset.
+"""
+
+import gzip
+from openeye import oechem
+
+# Highlight element of interest
+#subs = oechem.OESubSearch("[#6]") # carbon
+subs = None
+
+# Read the input SMILES
+filename = 'optimization_inputs.smi.gz'
+
+if filename.endswith(".gz"):
+    import gzip
+    with gzip.open(filename, 'r') as infile:
+        contents = infile.read().decode('utf-8')
+else:
+    with open(filename, 'r') as infile:
+        contents = infile.read()
+smiles_list = [ smiles for smiles in contents.splitlines() ]
+
+# Build OEMols
+oemols = list()
+for smiles in smiles_list:
+    oemol = oechem.OEMol()
+    oechem.OESmilesToMol(oemol, smiles)
+    oemols.append(oemol)
+
+# Generate a PDF of all molecules in the set
+pdf_filename = 'optimization_inputs.pdf'
+
+from openeye import oedepict
+itf = oechem.OEInterface()
+PageByPage = True
+suppress_h = True
+rows = 10
+cols = 6
+ropts = oedepict.OEReportOptions(rows, cols)
+ropts.SetHeaderHeight(25)
+ropts.SetFooterHeight(25)
+ropts.SetCellGap(2)
+ropts.SetPageMargins(10)
+report = oedepict.OEReport(ropts)
+cellwidth, cellheight = report.GetCellWidth(), report.GetCellHeight()
+opts = oedepict.OE2DMolDisplayOptions(cellwidth, cellheight, oedepict.OEScale_Default * 0.5)
+opts.SetAromaticStyle(oedepict.OEAromaticStyle_Circle)
+pen = oedepict.OEPen(oechem.OEBlack, oechem.OEBlack, oedepict.OEFill_On, 1.0)
+opts.SetDefaultBondPen(pen)
+oedepict.OESetup2DMolDisplayOptions(opts, itf)
+for i, mol in enumerate(oemols):
+    cell = report.NewCell()
+    mol_copy = oechem.OEMol(mol)
+    oedepict.OEPrepareDepiction(mol_copy, False, suppress_h)
+    disp = oedepict.OE2DMolDisplay(mol_copy, opts)
+
+    # Highlight element of interest
+    unique = False
+    if subs is not None:
+        for match in subs.Match(mol_copy, unique):
+            oedepict.OEAddHighlighting(disp, oechem.OEColor(oechem.OEYellow), oedepict.OEHighlightStyle_BallAndStick, match)
+
+    oedepict.OERenderMolecule(cell, disp)
+    #oedepict.OEDrawCurvedBorder(cell, oedepict.OELightGreyPen, 10.0)
+
+oedepict.OEWriteReport(pdf_filename, report)
