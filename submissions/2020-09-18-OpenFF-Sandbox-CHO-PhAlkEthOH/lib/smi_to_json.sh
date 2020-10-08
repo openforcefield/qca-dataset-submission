a4fe153d75b79662b91c9ed20f0408e3f371aa10
Trevor Gokey <trevorgokey@gmail.com>
2020-10-08 09:20:45 -0700

    update to use qcsubmit

--- submissions/2020-09-18-OpenFF-Sandbox-CHO-PhAlkEthOH/lib/smi_to_json.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-smi=$2
-cd $1
-python3 -u -m offsb.ui.smiles.load $smi -c 3 -n 100 -o ${smi}.log -f ${smi}.json --json | sed '/No principle axes found during inertial alignment/d'
-lzma -9v --force ${smi}.json
